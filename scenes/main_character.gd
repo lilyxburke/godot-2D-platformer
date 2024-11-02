4f4fdb84a5b5703212b2ee18f7e4b2beac2d5a68
lilyxburke <lburke701@gmail.com>
2024-11-02 20:29:22 +0000

    input map

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 400.0
+const SPEED = 300.0
 const JUMP_VELOCITY = -900.0
 @onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 
@@ -18,16 +18,16 @@ func _physics_process(delta: float) -> void:
 		sprite_2d.animation = "jumping"
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, 12)
+		velocity.x = move_toward(velocity.x, 0, 5)
 
 	move_and_slide()
 	
